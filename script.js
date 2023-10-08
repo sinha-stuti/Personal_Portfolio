0927a7bdf9539be3fcdaa08357e2c1f82b458593
Stuti Sinha <138568713+sinha-stuti@users.noreply.github.com>
2023-10-08 09:50:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+/* Typed .js */
+const typed = new Typed('.multiple-text', {
+    strings: ['Web Developer','Software Engineer','<Coder>'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 800,
+    loop: true
+});
